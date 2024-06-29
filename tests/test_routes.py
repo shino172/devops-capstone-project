06ad94c7ca9e885341643d341ad762597b4e2b9b
shino172 <anhhao24062004@gmail.com>
2024-06-29 15:02:39 -0400

    {commit message here}

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,35 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+
+        def test_get_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)       
+
+    ######################################################################
+    # READ AN ACCOUNT
+    ######################################################################
+    @app.route("/accounts/<int:account_id>", methods=["GET"])
+    def get_accounts(account_id):
+        """
+        Reads an Account
+        This endpoint will read an Account based the account_id that is requested
+        """
+        app.logger.info("Request to read an Account with id: %s", account_id)
+
+        account = Account.find(account_id)
+        if not account:
+            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+
+        return account.serialize(), status.HTTP_200_OK
+
+            def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
